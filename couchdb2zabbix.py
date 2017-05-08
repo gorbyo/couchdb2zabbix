1a7ffa564b8ec4e7db2e375521b3fd4711a2694e
Oleh Horbachov <gorbyo@gmail.com>
2017-05-08 16:27:16 +0300

    Move print to main function

--- couchdb2zabbix.py
+++ couchdb2zabbix.py
@@ -24,71 +24,67 @@ def get_active_tasks(url):
     active_tasks = json.load(response)
     return active_tasks
 
+''' 
+####### FOR FUTURE #######
+# def group_by_type(active_tasks):
+#     grouped_tasks_list = defaultdict(list)
+#
+#     for task_type in active_tasks:
+#         grouped_tasks_list[task_type['type']].append(task_type)
+#
+#     grouped_tasks = dict(grouped_tasks_list)
+#     return grouped_tasks
+#
+#
+# def count_grouped_tasks(grouped_tasks):
+#     count = {}
+#     for key, values in grouped_tasks.items():
+#         count[key] = len(values)
+#     return count
+'''
 
-def group_by_tasks(active_tasks):
-    grouped_tasks_list = defaultdict(list)
 
-    for task_type in active_tasks:
-        grouped_tasks_list[task_type['type']].append(task_type)
-
-    grouped_tasks = dict(grouped_tasks_list)
-    return grouped_tasks
-
-
-def count_tasks(grouped_tasks):
-    count = {}
-    for key, values in grouped_tasks.items():
-        count[key] = len(values)
-    return count
-
-
-def get_tasks(grouped_tasks):
-    count_dict = count_tasks(grouped_tasks)
+def get_tasks(active_tasks):
     tasks = {'data': []}
-    for key in grouped_tasks:
-        count = count_dict[key]
-        for val in range(0, count):
-            pid_norm = str(grouped_tasks[key][val]['pid']).translate(None, '<>')
+    count = len(active_tasks)
+    for val in range(0, count):
+            pid_norm = str(active_tasks[val]['pid']).translate(None, '<>')
             tasks['data'].append({"{#PID}": pid_norm})
-    print json.dumps(tasks)
+    return tasks
 
 
-def get_status(grouped_tasks, pid):
-    count_dict = count_tasks(grouped_tasks)
-    for key in grouped_tasks:
-        count = count_dict[key]
-        for val in range(0, count):
-            pid_str = str(grouped_tasks[key][val]['pid'])
-            if pid == pid_str.translate(None, '<>'):
-                if key == 'indexer' or key == 'database_compaction':
-                   status = grouped_tasks[key][val]['changes_done']
-                else:
-                   status = grouped_tasks[key][val]['progress']
-                print status
+def get_status(active_tasks, pid):
+    count = len(active_tasks)
+    for val in range(0, count):
+        pid_norm = str(active_tasks[val]['pid']).translate(None, '<>')
+        type_of_task = str(active_tasks[val]['type'])
+        if pid == pid_norm:
+            if type_of_task == 'indexer' or type_of_task == 'database_compaction':
+                status = str(active_tasks[val]['changes_done'])
+            else:
+                status = str(active_tasks[val]['progress'])
+            return status
 
 
-def get_type(grouped_tasks, pid):
-    count_dict = count_tasks(grouped_tasks)
-    for key in grouped_tasks:
-        count = count_dict[key]
-        for val in range(0, count):
-            pid_str = str(grouped_tasks[key][val]['pid'])
-            if pid == pid_str.translate(None, '<>'):
-                print grouped_tasks[key][val]['type']
+def get_type(active_tasks, pid):
+    count = len(active_tasks)
+    for val in range(0, count):
+        pid_norm = str(active_tasks[val]['pid']).translate(None, '<>')
+        if pid == pid_norm:
+            return str(active_tasks[val]['type'])
 
 
-def main(viewurl):
-    tasks_json = get_active_tasks(url=viewurl)
-    tasks_dict = group_by_tasks(tasks_json)
+def main(url):
+    tasks = get_active_tasks(url=url)
     if not (arguments().parse_args().pid is None):
         if (arguments().parse_args().type is None) or (arguments().parse_args().type == 'no'):
             pid = arguments().parse_args().pid
-            get_status(tasks_dict, pid)
+            print get_status(tasks, pid)
         elif arguments().parse_args().type == 'yes':
             pid = arguments().parse_args().pid
-            get_type(tasks_dict, pid)
+            print get_type(tasks, pid)
     else:
-        get_tasks(tasks_dict)
+        print json.dumps(get_tasks(tasks))
 
 
 if __name__ == '__main__':
